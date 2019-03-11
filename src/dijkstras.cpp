35d0ebf3019bbbd307913149d925f530e67d0773
Logan D Courtney <lcourtn5@hydra23.eecs.utk.edu>
2019-03-11 09:24:17 -0400

    First

---
+++ src/dijkstras.cpp
@@ -0,0 +1,21 @@
+// dijsktras.cpp
+
+
+//Node Class
+struct Node {
+	Node *backedge = NULL;
+	int distance = -1;
+	bool visited = false;
+}
+
+//Graph
+struct Graph {
+	multimap<Node, 
+}
+
+
+// Main Execution
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
